42027d7eb5bc53bd6368c1d91f63f0d68b8b492b
Discover Benix <mac@MACs-MacBook-Pro-3.local>
2026-06-29 23:35:42 +0100

    feat: add axios credential

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import axios from 'axios'
 import './index.css'
 import App from './App.tsx'
+
+// Set axios default to send cookies with credentials for cross-origin requests
+axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
